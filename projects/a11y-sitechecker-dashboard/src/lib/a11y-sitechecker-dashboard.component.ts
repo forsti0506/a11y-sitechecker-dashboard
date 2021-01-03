4e4b85970812387068e90f69e93a0b7d6555dd21
Martin Forstner <forsti@live.at>
2021-01-03 22:09:53 +0100

    Initial Files

---
+++ projects/a11y-sitechecker-dashboard/src/lib/a11y-sitechecker-dashboard.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-a11y-sitechecker-dashboard',
+  template: `
+    <p>
+      a11y-sitechecker-dashboard works!
+    </p>
+  `,
+  styles: []
+})
+export class A11ySitecheckerDashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ projects/a11y-sitechecker-dashboard/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of a11y-sitechecker-dashboard
+ */
+
+export * from './lib/a11y-sitechecker-dashboard.service';
+export * from './lib/a11y-sitechecker-dashboard.component';
+export * from './lib/a11y-sitechecker-dashboard.module';
