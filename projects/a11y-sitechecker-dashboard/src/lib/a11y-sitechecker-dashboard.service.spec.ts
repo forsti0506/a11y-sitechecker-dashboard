43aeb647c91888f177303b8cb33d584122a8350c
Martin Forstner <forsti@live.at>
2021-01-17 22:36:15 +0100

    fix: Tests fixed

--- projects/a11y-sitechecker-dashboard/src/lib/a11y-sitechecker-dashboard.service.spec.ts
+++ projects/a11y-sitechecker-dashboard/src/lib/a11y-sitechecker-dashboard.service.spec.ts
@@ -1,9 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { A11ySitecheckerDashboardService } from './a11y-sitechecker-dashboard.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('A11ySitecheckerDashboardService', () => {
-    beforeEach(() => TestBed.configureTestingModule({}));
+    beforeEach(() =>
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+            providers: [HttpClientTestingModule],
+        }),
+    );
 
     it('should be created', () => {
         const service: A11ySitecheckerDashboardService = TestBed.get(A11ySitecheckerDashboardService);
